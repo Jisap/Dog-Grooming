9052dd04dbf9917108dce0ce79e57aca726d9a90
Jorge Matos <jisapjisap@gmail.com>
2024-04-17 13:43:53 +0200

    HeroSlider

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import HeroSlider from '../components/HeroSlider'
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className='min-h-[748px] relative pt-24 flex items-center overflow-hidden lg:px-[50px] lg:pt-24 
+    after:lg:h-[740px] after:lg:w-[740px] after:lg:bg-orange-tertiary after:absolute after:lg:rounded-full
+    after:lg:-right-28 after:lg:-top-24'>
+      <HeroSlider />
+    </section>
+  )
 };
 
 export default Hero;
--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import '../swiper.css';
+import { Pagination, Navigation, Autoplay } from 'swiper'
+import { heroSlider } from '../data'
+
 
 const HeroSlider = () => {
-  return <div>HeroSlider</div>;
+  return (
+    <Swiper>
+      {heroSlider.map((slide, index) => {
+        const { title, subtitle, image, buttonText } = slide
+        return (
+          <SwiperSlide key={index}>
+            <div className='container mx-auto'>
+              <div>
+                <h1>{title}</h1>
+                <p>{subtitle}</p>
+                <button>button text</button>
+              </div>
+              <div>
+                <img src={image} />
+              </div>
+            </div>
+          </SwiperSlide>
+        )
+      })}
+    </Swiper>
+  )
 };
 
 export default HeroSlider;
