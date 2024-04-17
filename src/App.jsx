29be00bbfc73ce373780e47fbe8b3f280db7a8e3
Jorge Matos <jisapjisap@gmail.com>
2024-04-17 12:52:16 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import  Hero from './components/Hero'
+
+
+
+
+const App = () => {
+  return (
+    <div className="w-full max-w-[1440px] mx-auto bg-orange-quaternary relative">
+      <Hero />
+    </div>
+  )
+};
+
+export default App;
