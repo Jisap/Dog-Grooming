4410697e12814bac28a298ca7b8b26e22e3032ee
Jorge Matos <jisapjisap@gmail.com>
2024-04-18 23:19:09 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
     <div className="w-full max-w-[1440px] mx-auto bg-orange-quaternary relative">
       <Header />
       <Hero />
+      <div className="h-[2000px]"></div>
     </div>
   )
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Logo from "../assets/img/logo.svg";
+import { CgMenuRight } from "react-icons/cg";
+import  Nav from "./Nav";
+import NavMobile from "./NavMobile";
+import Program from "./Program";
 
 const Header = () => {
-  return <div>Header</div>;
+  // header bg state
+  const [bg, setBg] = useState(false);
+  
+  // nav mobile state
+  const [navMobile, setNavMobile] = useState(false);
+  
+  //scroll event
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 50 ? setBg(true) : setBg(false)
+    })
+  })
+  
+  return (
+
+    <header 
+      className={`${
+        bg 
+          ? 'bg-white py-3 shadow-md'
+          : 'bg-none py-5'
+      } fixed w-full right-0 left-0 z-10 transition-all duration-300`}
+    >
+      <div className="container mx-auto flex justify-between items-center">
+        {/* logo */}
+        <a href='#'>
+          <img src={Logo} alt='' />
+        </a>
+        {/* nav */}
+        <div className="hidden lg:flex">
+          <Nav />
+        </div>
+        {/* program */}
+        <Program />
+        {/* nav mobile button*/}
+        <div className="lg:hidden cursor-pointer">
+          <CgMenuRight className="text-blue text-3xl" />
+        </div>
+      </div>
+    </header>
+  )
 };
 
 export default Header;
